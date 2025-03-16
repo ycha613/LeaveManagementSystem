9d0150ea969b701c34821ee20c41e83a7ead3dfe
ycha613 <ycha613@aucklanduni.ac.nz>
2025-03-16 18:35:19 +1300

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2003, 10, 30)
+            };
+            return View(data);
+        }
+    }
+}
